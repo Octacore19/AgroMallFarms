25973f760ab147db0564ee724964292daf383a2b
Chukwu Gideon Emeka <chuksgid@gmail.com>
2020-05-25 20:19:45 +0100

    Cleared some unneeded fields and components

--- app/src/main/java/com/octacore/agromallfarms/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/octacore/agromallfarms/ui/register/RegisterFragment.kt
@@ -1,4 +1,4 @@
-package com.octacore.agromallfarms.ui.gallery
+package com.octacore.agromallfarms.ui.register
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,7 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.octacore.agromallfarms.R
 
-class GalleryFragment : Fragment() {
+class RegisterFragment : Fragment() {
 
     private lateinit var galleryViewModel: GalleryViewModel
 
@@ -21,7 +21,7 @@ class GalleryFragment : Fragment() {
     ): View? {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val root = inflater.inflate(R.layout.fragment_register, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
